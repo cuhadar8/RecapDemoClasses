b75fc13b57f3ee1bd1776e48e74e58079559423d
cuhadar8 <ibrahimcuhadar8@gmail.com>
2022-10-05 20:05:42 +0300

    RecapDemoClasses

---
+++ RecapDemoClasses/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		
+		int sayi = dortIslem.Topla(5, 10);
+		System.out.println(sayi);
+	}
+
+}
